5e1b59a2eced9d85ba7c2d7b27d42c1508bf2ea0
auroramarch <auroramb90@gmail.com>
2017-07-20 17:36:11 +0200

    iterance 1 done

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,24 +1,47 @@
-(function($) {
+var $button = $('.sentId');
 
-	var $button = $('.sentId');
+$button.on("click", function(e) {
+		$('.name').empty();
+		$('.type').empty();
+		$('.description').empty();
+		$('.pokemon-image').empty();
+		var $inputValue = $('.searchPokemon').val();
 
-	$button.on("click", function(e) {
-  		$('.name').empty();
-  		$('.type').empty();
-  		$('.description').empty();
-  		var $inputValue = $('.searchPokemon').val();
+$.ajax({
+   	url: 'http://pokeapi.co/api/v2/pokemon/'+ $inputValue+'/', 
+   	dataType: 'json',
+   success: pokemons
+});
 
-	$.ajax({
-	   	url: 'http://pokeapi.co/api/v2/pokemon/'+ $inputValue+'/', 
-	   	dataType: 'json',
-	   success: pokemons
-	});
-
-	function pokemons(response){
-		console.log(response);
-		$('.name').append(response.name);
+function pokemons(response){
+	$('.pokemon').before("<img class='card-img-top pokemon-image' src='" + response.sprites.front_default + "' alt='Image Pokemon'>");
+	$('.name').append(response.name);
+	if (response.types.length>1){
+		$('.type').append(response.types[0].type.name);
+		$('.type').append(' '+response.types[1].type.name);
+	} else {
+		$('.type').append(response.types[0].type.name);
 	}
 
-	});
+	var $pokemonId= response.id;
+	console.log($pokemonId);
+	
+	$.get ('http://pokeapi.co/api/v1/pokemon/' + $pokemonId, descriptions);
 
-})(jQuery);
+	function descriptions (pokeDescription){
+		$.get ('http://pokeapi.co' + pokeDescription.resource_uri, textDescription);
+	}
+	function textDescription (data) {
+		$.get ('http://pokeapi.co' + data.descriptions[0].resource_uri, description);
+	}
+
+	function description (dataDescription){
+		$('.description').append(dataDescription.description);
+	}
+
+
+}
+
+
+});
+	
